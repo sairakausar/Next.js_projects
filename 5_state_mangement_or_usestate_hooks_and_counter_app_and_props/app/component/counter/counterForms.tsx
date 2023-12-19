ccb66e256d25d4ce288d26f2b1238f39e42e30f5
sairakausar <sairakausar3@gmail.com>
2023-12-18 23:02:30 -0800

    update

---
+++ 5_state_mangement_or_usestate_hooks_and_counter_app_and_props/app/component/counter/counterForms.tsx
@@ -0,0 +1,21 @@
+ 'use client'
+import React from 'react'
+import Button from './button'
+
+export default function CounterForms() {
+    return (
+      <>
+        
+        {/* // This tage name is fragment tag. or React fragment. */}
+        <div>
+                This is use for the name change with the use state or
+                 power ful hook   or power hook and First made button
+     
+            <Button/>
+     
+        </div>
+            
+            
+      </>
+    );
+}
---
+++ 5_state_mangement_or_usestate_hooks_and_counter_app_and_props/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import CounterForms from './component/counter/counterForms'
+
+export default function Home() {
+  return (
+    <main>
+
+<CounterForms/>
+
+    </main>
+  )
+}
