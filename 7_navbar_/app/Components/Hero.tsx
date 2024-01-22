c540ed4fd5825f64b214f0e2f3d30e322fc834e7
sairakausar <sairakausar3@gmail.com>
2024-01-22 08:26:39 -0800

    Hero Section in process

--- 7_navbar_/app/Components/Hero.tsx
+++ 7_navbar_/app/Components/Hero.tsx
@@ -6,28 +6,38 @@ import {
     ServerIcon
 } from '@heroicons/react/solid'
 import bgImg from '../assets/cyber-bg.png'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className="w-ful h-screen bg-zinc-200 flex flex-col justify-between">
-      <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
-        <div>
-          <p>Unique Sequencing & Production</p>
-          <h1>Cloud Management</h1>
-          <p>This is Our Tech brand. </p>
-          <button>Get Started</button>
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl">Unique Sequencing & Production</p>
+          <h1 className="py-3 text-5xl md:text-6xl font-bold">
+            Cloud Management
+          </h1>
+          <p className="text-2xl">This is Our Tech brand. </p>
+          <button className="py-3 px-6 sm:w-[60%] my-4 w-52 hover:text-5xl hover:font-bold hover:w-96 hover:bg-zinc-400">
+            Get Started
+          </button>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        Hero Section
+        <div>
+          <Image className="w-full" src={bgImg} alt="image" />
+          {/* // this is extention // for boxes // pesticide for Chrome// */}
+        </div>
+        <div>
+          <p>
+            <div>Data Service</div>
+            <div>
+              <p>Icon. App Security </p>
+              <p>Icon. Dashboard Design </p>
+              <p>Icon. Cloud Data </p>
+              <p>Icon. API </p>
+            </div>
+          </p>
+        </div>
       </div>
-      <div>
-        Hero two
-      </div>
-      <div>Hero Three</div>
     </div>
   );
 }
