e9db082001752a39871ccc69a7f2fe8440fbe68d
sairakausar <sairakausar3@gmail.com>
2024-01-27 11:57:39 -0800

    fram.. like text Animate

--- 9_framer_motion_start/app/component/FramerMotion.tsx
+++ 9_framer_motion_start/app/component/FramerMotion.tsx
@@ -7,7 +7,14 @@ export default function FramerMotion() {
         <motion.h1
         
             initial={{ x: -100 }}
-        animate={{x: 0 }}>
+            animate={{ x: 0 }}
+            transition={{
+                duration: "2",
+                delay: "1"
+                
+            }}
+        className="text-green-600 text-5xl"
+        >
             It is H1 for animation using Framer Motion 
         </motion.h1>
     )
