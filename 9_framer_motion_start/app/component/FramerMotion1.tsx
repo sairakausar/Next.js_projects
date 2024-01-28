2a3d39ed6407a27f33784c4fceee13b88b99a38b
sairakausar <sairakausar3@gmail.com>
2024-01-28 06:27:30 -0800

    fram.. box move

--- 9_framer_motion_start/app/component/FramerMotion1.tsx
+++ 9_framer_motion_start/app/component/FramerMotion1.tsx
@@ -7,14 +7,17 @@ export default function FramerMotion1() {
         <motion.h1
         
             initial={{ x: 0 }}
-            animate={{ x: [ 0 , 900 , 0 ] }}
+            // initial start from 0    in x-xix
+            animate={{ x: [0, 900, 0] }}
+            // it start from 0 and till 900 and also reach again in 0
             transition={{
                 duration: 3,
                 delay: 0.3
                 
             }}
             className="text-5xl text-lime-600"
-            whileHover= {{scale: 0.9, opacity: 0.2}}
+            whileHover={{ scale: 0.9, opacity: 0.2 }}
+            //scale mean Size , Opacity mean Opacity when Hover
         >
             It is H1 for animation using Framer Motion 
         </motion.h1>
--- 9_framer_motion_start/app/page.tsx
+++ 9_framer_motion_start/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import FramerMotion1 from "./component/FramerMotion1";
+import FramerMotion2 from "./component/FramerMotion2";
 
 export default function Home() {
   return (
     <main >
-<FramerMotion1 />
+      <FramerMotion1 />
+      <FramerMotion2 />
 
     </main>
   );
