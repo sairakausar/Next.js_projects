a485202bcf898d92d7eb87d0a43aa5cd01fbe71b
sairakausar <sairakausar3@gmail.com>
2024-01-28 12:17:13 -0800

    fram.. update

--- 9_framer_motion_start/app/component/FramerMotion2.tsx
+++ 9_framer_motion_start/app/component/FramerMotion2.tsx
@@ -5,30 +5,34 @@ import { useState } from "react";
 export default function FramerMotion2() {
     const [show, setShow] = useState(true)
   return (
-      <div
-          style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "10px",
-              alignItems: "center"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        alignItems: "center",
       }}
-      >
-          <h1 style={{marginTop: "30px" }} className="text-pink-800 ">Framer Motion Tutorial: Exit and Enter animation</h1>
-          <AnimatePresence>
-              {
-                  show && <motion.div
-                      style={{ width: "100px", height: "100px", background: "blue"}}
-                      initial={{ opacity: 0, x: 50 }}
-                      animate={{ opacity: 1, x: 100 }}
-                      exit={{ opacity: 0, x: -150 }}
-                      transition={{
-                          duration: "1"
-                      }}
-                  >    
-                  </motion.div>
-              }
-          </AnimatePresence>
-          <button onClick={()=> setShow(!show)} style={{padding: "12px 18px"}}> {show?"Remove Box": "Add Box"} </button>
+    >
+      <h1 style={{ marginTop: "30px" }} className="text-pink-800 text-5xl">
+        (FramerMotion2) : Exit and Enter animation
+      </h1>
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            style={{ width: "100px", height: "100px", background: "blue" }}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 100 }}
+            exit={{ opacity: 0, x: -150 }}
+            transition={{
+              duration: "1",
+            }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+      <button onClick={() => setShow(!show)} style={{ padding: "12px 18px" }}>
+    
+        {show ? "Remove Box" : "Add Box"}
+      </button>
     </div>
   );
 }
